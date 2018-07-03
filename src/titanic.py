4b5f3bc41fc649b07b2cb3db261369673004b628
Teodor Moroz <teomoroz@gmail.com>
2018-07-03 08:01:01 +0300

    Added test prediction functionality

--- src/titanic.py
+++ src/titanic.py
@@ -10,9 +10,36 @@ from sklearn.model_selection import cross_val_score, cross_val_predict
 
 DATA_PATH = '../data/'
 
+
 def load_dataset(dataset_path):
     csv_path = os.path.join(DATA_PATH, dataset_path)
     return pd.read_csv(csv_path)
+
+
+def get_feature_matrix(dataset):
+    dataset['Sex'] = dataset['Sex'].map(lambda x: 1 if x=='male' else 0)
+    dataset['Age'] = dataset['Age'].fillna(dataset['Age'].mean());
+    dataset['Fare'] = dataset['Fare'].fillna(dataset['Fare'].mean());
+
+    dataset['Embarked'] = dataset['Embarked'].map(lambda x: 0 if x == 'C' else 1 if x == 'Q' else 2)
+    #dataset['Age'] = dataset['Age'].fillna(0);
+    feature_matrix = dataset.as_matrix(columns=['Pclass', 'Embarked', 'Fare', 'Sex', 'Age'])
+    print(dataset.isnull().any())
+    return feature_matrix
+
+def create_prediction_file(test_dataset, prediction):
+    df = pd.DataFrame({'PassengerId':test_dataset['PassengerId'], 'Survived':prediction})
+    df.to_csv('../data/predict_res.csv', index=False)
+
+
+def test_prediction(random_forest):
+    test_dataset = load_dataset('test.csv')
+    feature_matix = get_feature_matrix(test_dataset)
+    print ("Prediction on unseen data")
+    predict_result = random_forest.predict(feature_matix)
+    create_prediction_file(test_dataset, predict_result)
+
+
 
 #probably the most correlation is due to passanger class, sex and age
 def main():
@@ -21,13 +48,7 @@ def main():
     #print(train_dataset['PassengerId'])
     # plt.scatter(train_dataset["Age"], train_dataset["Fare"], c=train_dataset["Survived"]);
     # plt.show();
-    #train_dataset = train_dataset.apply(lambda x: x == 'male', axis=1)
-    train_dataset['Sex'] = train_dataset['Sex'].map(lambda x: 1 if x=='male' else 0)
-    train_dataset['Age'] = train_dataset['Age'].fillna(train_dataset['Age'].mean());
-
-    train_dataset['Embarked'] = train_dataset['Embarked'].map(lambda x: 0 if x == 'C' else 1 if x == 'Q' else 2)
-    #train_dataset['Age'] = train_dataset['Age'].fillna(0);
-    feature_matrix = train_dataset.as_matrix(columns=['Pclass', 'Embarked', 'Fare', 'Sex', 'Age'])
+    feature_matrix = get_feature_matrix(train_dataset)
     print(feature_matrix)
     feature_output = train_dataset.as_matrix(columns=['Survived'])
 
@@ -51,6 +72,7 @@ def main():
     survive_prob = cross_val_predict(random_forest, feature_matrix, feature_output, cv=3)
     #print(survive_prob)
 
+    test_prediction(random_forest)
     #TODO: probably some additional feature analysis or engineering
 
 
