a8abd7467660a24743432e2ac1788455b64d2cfe
Teodor Moroz <teomoroz@gmail.com>
2018-07-10 08:13:28 +0300

    Added grid search

--- src/titanic.py
+++ src/titanic.py
@@ -8,6 +8,9 @@ from sklearn.linear_model import SGDClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.kernel_approximation import RBFSampler
 from sklearn.model_selection import cross_val_score, cross_val_predict
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import CategoricalEncoder
+from sklearn.model_selection import GridSearchCV
 
 DATA_PATH = '../data/'
 
@@ -22,6 +25,17 @@ def substrings_in_string(big_string, substrings):
         if big_string.find(curr_str) != -1:
             return curr_str
     return np.nan
+
+
+def encode_categories(dataset, column_name):
+    # one_hot = pd.get_dummies(dataset[column_name])
+    # dataset = dataset.drop(column_name, axis=1)
+    # dataset = dataset.join(one_hot)
+    # print('ROCKKKKKKKKKKKKKKKKK-', one_hot)
+    dataset = pd.concat([dataset, pd.get_dummies(dataset['Title'], prefix='Title')], axis=1)
+    print(dataset.head())
+    return dataset
+
 
 
 def get_tittle(dataset):
@@ -47,26 +61,13 @@ def get_tittle(dataset):
             return title
     dataset['Title'] = dataset.apply(replace_titles, axis=1)
 
-    def title_to_num(title):
-        if title == 'Mrs':
-            return 0
-        elif title =='Miss':
-            return 1
-        elif title == 'Officer':
-            return 2
-        elif title == 'Mr':
-            return 3
-        else:
-            print("Invalid title", title)
-            return 10
-
-    dataset['Title'] = dataset['Title'].map(title_to_num);
     return dataset['Title']
 
 
 #TODO: fare split on members number
 def get_feature_matrix(dataset):
     dataset['Title'] = get_tittle(dataset)
+    dataset = encode_categories(dataset, 'Title')
     dataset['Sex'] = dataset['Sex'].map(lambda x: 1 if x=='male' else 0)
     dataset['Age'] = dataset['Age'].fillna(dataset['Age'].mean());
     dataset['Fare'] = dataset['Fare'].fillna(dataset['Fare'].mean());
@@ -78,11 +79,15 @@ def get_feature_matrix(dataset):
     print("COUNT - ", dataset['FamilySize'].value_counts(ascending=True))
     #print("COUNT - ", dataset['ActualFare'].value_counts(ascending=True))
 
-    dataset['Embarked'] = dataset['Embarked'].map(lambda x: 0 if x == 'C' else 1 if x == 'Q' else 2)
+    #dataset['Embarked'] = dataset['Embarked'].map(lambda x: 0 if x == 'C' else 1 if x == 'Q' else 2)
+    dataset = pd.concat([dataset, pd.get_dummies(dataset['Embarked'], prefix='Embarked')], axis=1)
+    print(list(dataset))
     #dataset['Age'] = dataset['Age'].fillna(0);
-    feature_matrix = dataset.as_matrix(columns=['Pclass', 'Sex', 'Title', 'Fare', 'Age', 'Embarked', 'FamilySize' ])
+    feature_matrix = dataset.as_matrix(columns=['Pclass', 'Sex', 'Fare', 'ActualFare', 'Age', 'FamilySize',
+                                                'Title_Officer', 'Title_Mrs', 'Title_Mr', 'Title_Miss'])
     print(dataset.isnull().any())
     return feature_matrix
+
 
 def create_prediction_file(test_dataset, prediction):
     df = pd.DataFrame({'PassengerId':test_dataset['PassengerId'], 'Survived':prediction})
@@ -106,6 +111,17 @@ def plot_histogram(x, y):
     axs[1].hist(y, bins=n_bins)
     plt.show()
 
+def hyper_param_search(feature_matrix, feature_output):
+    param_grid = [
+        {'n_estimators': [3, 10, 30], 'max_features': [2, 4, 6, 8, 10]},
+        {'bootstrap': [False], 'n_estimators': [3, 10], 'max_features': [2, 3, 4]}
+    ]
+    forest_reg = RandomForestClassifier();
+    grid_search = GridSearchCV(forest_reg, param_grid, cv=5, scoring='neg_mean_squared_error')
+    grid_search.fit(feature_matrix, feature_output)
+    print(grid_search.best_params_)
+    return grid_search
+
 
 #probably the most correlation is due to passanger class, sex and age
 def main():
@@ -128,15 +144,17 @@ def main():
      #print(feature_matrix)
     #class_weights = {0:3, 1:1, 2:1, 3:3, 4:2}
     #class_weights = {0: 3, 1: 1}
-    random_forest = RandomForestClassifier(random_state=42)
-    print (random_forest.fit(feature_matrix, feature_output))
-    val_score = cross_val_score(random_forest, feature_matrix, feature_output, cv=3, scoring="accuracy");
+    grid_search = hyper_param_search(feature_matrix, feature_output)
+    final_model = grid_search.best_estimator_
+    random_forest = RandomForestClassifier(random_state=42,  n_estimators=50)
+    print (final_model.fit(feature_matrix, feature_output))
+    val_score = cross_val_score(final_model, feature_matrix, feature_output, cv=3, scoring="accuracy");
     print(val_score)
-    survive_prob = cross_val_predict(random_forest, feature_matrix, feature_output, cv=3)
+    survive_prob = cross_val_predict(final_model, feature_matrix, feature_output, cv=3)
     #print(survive_prob)
 
-    test_prediction(random_forest)
-    #TODO: probably some additional feature analysis or engineering, try to dropna
+    test_prediction(final_model)
+    #TODO:try to use onehotencoder and categoricalencoder
 
 
 if __name__ == "__main__":
