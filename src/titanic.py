1cd41cd9be233b94628c437fa4c0f4577433688f
Teodor Moroz <teomoroz@gmail.com>
2018-07-11 08:03:03 +0300

    Addded cross-validation check

--- src/titanic.py
+++ src/titanic.py
@@ -11,6 +11,7 @@ from sklearn.model_selection import cross_val_score, cross_val_predict
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.preprocessing import CategoricalEncoder
 from sklearn.model_selection import GridSearchCV
+from sklearn.linear_model import LogisticRegression
 
 DATA_PATH = '../data/'
 
@@ -48,10 +49,8 @@ def get_tittle(dataset):
         title=x['Title']
         if title in ['Don', 'Major', 'Capt', 'Jonkheer', 'Rev', 'Col', 'Master']:
             return 'Officer'
-        elif title in ['Countess', 'Mme']:
+        elif title in ['Countess', 'Mme', 'Mrs', 'Miss', 'Mlle', 'Ms']:
             return 'Mrs'
-        elif title in ['Mlle', 'Ms']:
-            return 'Miss'
         elif title =='Dr':
             if x['Sex']=='Male':
                 return 'Mr'
@@ -68,24 +67,24 @@ def get_tittle(dataset):
 def get_feature_matrix(dataset):
     dataset['Title'] = get_tittle(dataset)
     dataset = encode_categories(dataset, 'Title')
-    dataset['Sex'] = dataset['Sex'].map(lambda x: 1 if x=='male' else 0)
+    dataset['Sex'] = dataset['Sex'].map(lambda x: 1 if x=='female' else 0)
     dataset['Age'] = dataset['Age'].fillna(dataset['Age'].mean());
     dataset['Fare'] = dataset['Fare'].fillna(dataset['Fare'].mean());
 
     dataset['FamilySize'] = dataset['SibSp'] + dataset['Parch'] + 1
     dataset['ActualFare'] = dataset['Fare'] / (dataset['FamilySize'])
     dataset['ActualFare'] = dataset['ActualFare'].fillna(dataset['ActualFare'].mean())
+    dataset['IsChild'] = dataset['Age'] <= 16
 
     print("COUNT - ", dataset['FamilySize'].value_counts(ascending=True))
     #print("COUNT - ", dataset['ActualFare'].value_counts(ascending=True))
 
-    #dataset['Embarked'] = dataset['Embarked'].map(lambda x: 0 if x == 'C' else 1 if x == 'Q' else 2)
     dataset = pd.concat([dataset, pd.get_dummies(dataset['Embarked'], prefix='Embarked')], axis=1)
     print(list(dataset))
-    #dataset['Age'] = dataset['Age'].fillna(0);
-    feature_matrix = dataset.as_matrix(columns=['Pclass', 'Sex', 'Fare', 'ActualFare', 'Age', 'FamilySize',
-                                                'Title_Officer', 'Title_Mrs', 'Title_Mr', 'Title_Miss'])
-    print(dataset.isnull().any())
+    feature_matrix = dataset.as_matrix(columns=['Pclass', 'Sex', 'ActualFare', 'Age', 'FamilySize',
+                                                'Title_Officer', 'Title_Mrs', 'Title_Mr',
+                                                'Embarked_Q', 'Embarked_S', 'Embarked_C'])
+    print(feature_matrix)
     return feature_matrix
 
 
@@ -103,6 +102,8 @@ def test_prediction(random_forest):
 
 
 def plot_histogram(x, y):
+    x.dropna(inplace=True)
+    y.dropna(inplace=True)
     fig, axs = plt.subplots(1, 2, sharey=True, tight_layout=True)
 
     n_bins=20
@@ -113,14 +114,26 @@ def plot_histogram(x, y):
 
 def hyper_param_search(feature_matrix, feature_output):
     param_grid = [
-        {'n_estimators': [3, 10, 30], 'max_features': [2, 4, 6, 8, 10]},
+        {'n_estimators': [3, 10, 30], 'max_features': [2, 4, 6, 8, 10, 12]},
         {'bootstrap': [False], 'n_estimators': [3, 10], 'max_features': [2, 3, 4]}
     ]
-    forest_reg = RandomForestClassifier();
+    forest_reg = RandomForestClassifier(random_state=42,  n_estimators=50);
     grid_search = GridSearchCV(forest_reg, param_grid, cv=5, scoring='neg_mean_squared_error')
     grid_search.fit(feature_matrix, feature_output)
     print(grid_search.best_params_)
     return grid_search
+
+
+def display_scores(scores):
+    print("Scores:", scores)
+    print("Mean:", scores.mean())
+    print("Standard deviation:", scores.std())
+
+
+def compute_scores(model, feature_matrix, feature_output):
+    scores = cross_val_score(model, feature_matrix, feature_output, scoring="neg_mean_squared_error", cv=10)
+    tree_rmse_scores = np.sqrt(-scores)
+    display_scores(tree_rmse_scores)
 
 
 #probably the most correlation is due to passanger class, sex and age
@@ -138,23 +151,28 @@ def main():
 
     feature_matrix = get_feature_matrix(train_dataset)
     print(feature_matrix)
-   # plot_histogram(train_dataset['Age'], train_dataset['Survived'])
+    plot_histogram(train_dataset['Age'], train_dataset['Survived'])
     feature_output = train_dataset.as_matrix(columns=['Survived'])
 
      #print(feature_matrix)
-    #class_weights = {0:3, 1:1, 2:1, 3:3, 4:2}
+    class_weights = {0:3, 1:1, 2:1, 3:3, 4:2}
     #class_weights = {0: 3, 1: 1}
-    grid_search = hyper_param_search(feature_matrix, feature_output)
-    final_model = grid_search.best_estimator_
-    random_forest = RandomForestClassifier(random_state=42,  n_estimators=50)
+    # grid_search = hyper_param_search(feature_matrix, feature_output)
+    # final_model = grid_search.best_estimator_
+    final_model = RandomForestClassifier(random_state=42, n_estimators=100,oob_score=True,max_features=5)
+    #final_model = LogisticRegression()
     print (final_model.fit(feature_matrix, feature_output))
+    #print(list(zip(, final_model.feature_importances_)))
+    #print(final_model.feature_importances_)
     val_score = cross_val_score(final_model, feature_matrix, feature_output, cv=3, scoring="accuracy");
     print(val_score)
+    print(final_model.score(feature_matrix, feature_output))
+    compute_scores(final_model, feature_matrix, feature_output)
     survive_prob = cross_val_predict(final_model, feature_matrix, feature_output, cv=3)
     #print(survive_prob)
 
-    test_prediction(final_model)
-    #TODO:try to use onehotencoder and categoricalencoder
+#    test_prediction(final_model)
+    #TODO: add cross-validation
 
 
 if __name__ == "__main__":
