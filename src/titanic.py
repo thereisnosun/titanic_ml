33cd0e39581997e928b46bfc371f0454053e55c9
Teodor Moroz <teomoroz@gmail.com>
2018-07-23 08:33:43 +0300

    Adding voting classifier

--- src/titanic.py
+++ src/titanic.py
@@ -12,6 +12,9 @@ from sklearn.preprocessing import OneHotEncoder
 from sklearn.preprocessing import CategoricalEncoder
 from sklearn.model_selection import GridSearchCV
 from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.ensemble import VotingClassifier
 
 DATA_PATH = '../data/'
 
@@ -29,10 +32,6 @@ def substrings_in_string(big_string, substrings):
 
 
 def encode_categories(dataset, column_name):
-    # one_hot = pd.get_dummies(dataset[column_name])
-    # dataset = dataset.drop(column_name, axis=1)
-    # dataset = dataset.join(one_hot)
-    # print('ROCKKKKKKKKKKKKKKKKK-', one_hot)
     dataset = pd.concat([dataset, pd.get_dummies(dataset['Title'], prefix='Title')], axis=1)
     print(dataset.head())
     return dataset
@@ -74,16 +73,16 @@ def get_feature_matrix(dataset):
     dataset['FamilySize'] = dataset['SibSp'] + dataset['Parch'] + 1
     dataset['ActualFare'] = dataset['Fare'] / (dataset['FamilySize'])
     dataset['ActualFare'] = dataset['ActualFare'].fillna(dataset['ActualFare'].mean())
-    dataset['IsChild'] = dataset['Age'] <= 16
+    dataset['AgeCategory'] = dataset['Age'].map(lambda x: 0 if x <= 16 else 2 if x>=64 else 1)
 
     print("COUNT - ", dataset['FamilySize'].value_counts(ascending=True))
     #print("COUNT - ", dataset['ActualFare'].value_counts(ascending=True))
 
     dataset = pd.concat([dataset, pd.get_dummies(dataset['Embarked'], prefix='Embarked')], axis=1)
     print(list(dataset))
-    feature_matrix = dataset.as_matrix(columns=['Pclass', 'Sex', 'ActualFare', 'Age', 'FamilySize',
-                                                'Title_Officer', 'Title_Mrs', 'Title_Mr',
-                                                'Embarked_Q', 'Embarked_S', 'Embarked_C'])
+    feature_matrix = dataset.as_matrix(columns=['Fare', 'Pclass', 'Sex',  'Age',   'FamilySize',
+                                                 'Title_Officer', 'Title_Mrs', 'Title_Mr',
+                                                 'Embarked_Q', 'Embarked_S', 'Embarked_C', 'AgeCategory'])
     print(feature_matrix)
     return feature_matrix
 
@@ -97,6 +96,8 @@ def test_prediction(random_forest):
     test_dataset = load_dataset('test.csv')
     feature_matix = get_feature_matrix(test_dataset)
     print ("Prediction on unseen data")
+    # sc = StandardScaler()
+    # sc.fit_transform(feature_matix)
     predict_result = random_forest.predict(feature_matix)
     create_prediction_file(test_dataset, predict_result)
 
@@ -114,10 +115,10 @@ def plot_histogram(x, y):
 
 def hyper_param_search(feature_matrix, feature_output):
     param_grid = [
-        {'n_estimators': [3, 10, 30], 'max_features': [2, 4, 6, 8, 10, 12]},
+        {'n_estimators': [3, 10, 30], 'max_features': [2, 4, 6, 8, 10, 11]},
         {'bootstrap': [False], 'n_estimators': [3, 10], 'max_features': [2, 3, 4]}
     ]
-    forest_reg = RandomForestClassifier(random_state=42,  n_estimators=50);
+    forest_reg = RandomForestClassifier(random_state=42,  n_estimators=500, n_jobs=-1);
     grid_search = GridSearchCV(forest_reg, param_grid, cv=5, scoring='neg_mean_squared_error')
     grid_search.fit(feature_matrix, feature_output)
     print(grid_search.best_params_)
@@ -150,28 +151,35 @@ def main():
 
 
     feature_matrix = get_feature_matrix(train_dataset)
-    print(feature_matrix)
-    plot_histogram(train_dataset['Age'], train_dataset['Survived'])
+    # print(feature_matrix)
+    # plot_histogram(train_dataset['Age'], train_dataset['Survived'])
     feature_output = train_dataset.as_matrix(columns=['Survived'])
 
      #print(feature_matrix)
-    class_weights = {0:3, 1:1, 2:1, 3:3, 4:2}
+    #class_weights = {0:3, 1:1, 2:1, 3:3, 4:2}
     #class_weights = {0: 3, 1: 1}
     # grid_search = hyper_param_search(feature_matrix, feature_output)
     # final_model = grid_search.best_estimator_
-    final_model = RandomForestClassifier(random_state=42, n_estimators=100,oob_score=True,max_features=5)
-    #final_model = LogisticRegression()
-    print (final_model.fit(feature_matrix, feature_output))
+    #final_model = RandomForestClassifier(random_state=42, n_estimators=1000,max_features=10)
+    random_forest = RandomForestClassifier(random_state=42, n_estimators=1000, max_features=10)
+    extra_clf = ExtraTreesClassifier(random_state=13, n_estimators=500, min_samples_split=1.0, max_depth=None)
+    log_clf = LogisticRegression()
+
+    final_model = VotingClassifier(
+        estimators=[('lr', log_clf), ('rf', random_forest), ('extr', extra_clf)],
+        voting='hard')
+
+    final_model.fit(feature_matrix, feature_output)
     #print(list(zip(, final_model.feature_importances_)))
     #print(final_model.feature_importances_)
-    val_score = cross_val_score(final_model, feature_matrix, feature_output, cv=3, scoring="accuracy");
+    val_score = cross_val_score(final_model, feature_matrix, feature_output, cv=3, scoring="accuracy").mean();
     print(val_score)
-    print(final_model.score(feature_matrix, feature_output))
+    #print(final_model.score(feature_matrix, feature_output))
     compute_scores(final_model, feature_matrix, feature_output)
-    survive_prob = cross_val_predict(final_model, feature_matrix, feature_output, cv=3)
+    #survive_prob = cross_val_predict(final_model, feature_matrix, feature_output, cv=3)
     #print(survive_prob)
 
-#    test_prediction(final_model)
+    test_prediction(final_model)
     #TODO: add cross-validation
 
 
